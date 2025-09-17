59f96add54f871c816b529baffd7c297ea7afbe4
Ramsen <ramsensliyo@gmail.com>
2025-09-17 14:38:52 +0200

    Push pull legs type push

--- app.py
+++ app.py
@@ -1,25 +1,79 @@
 import sqlite3
 from flask import Flask, jsonify, render_template
+import time
+import threading
+import serial
+
 
 # Initialize Flask app
 app = Flask(__name__)
+
+# Sæt COM-porten til den hvor Arduino er tilsluttet
+arduino_port = "COM5"
+baud_rate = 115200
+co2 = ""
+temp = ""
+humidity = ""
+
+
+# Opret forbindelse til Arduino
+ser = serial.Serial(arduino_port, baud_rate, timeout=2)
+time.sleep(2)  # vent på at Arduino resetter
+
+lokaler_data = [
+    {"klasse": "Nuværende placering", "co2": 0, "temperature": 0, "luftfugtighed": 0},
+    {"klasse": "D2321", "co2": 1111, "temperature": 22.55, "luftfugtighed": 55.12},
+    {"klasse": "D2349", "co2":1004, "temperature": 21.44, "luftfugtighed": 49.69}
+]
+
+def baggrundsopgave():
+    try:
+        while True:
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').strip()
+                if line:
+                    if line.__contains__("CO2 concentration [ppm]: "):
+                        co2 = line[25:]
+                        print(co2)
+                        lokaler_data[0]["co2"] = co2
+                    elif line.__contains__("Temperature [°C]: "):
+                        temp = line[18:]
+                        print(temp)
+                        lokaler_data[0]["temperature"] = temp
+                    elif line.__contains__("Relative Humidity [RH]: "):
+                        humidity = line[24:]
+                        print(humidity)
+                        lokaler_data[0]["luftfugtighed"] = humidity
+                    print(line)  # udskriver alt Arduino sender
+
+
+
+    except KeyboardInterrupt:
+        print("Afslutter...")
+    finally:
+        ser.close()
+
+
+# Start baggrundstråden før Flask kører
+thread = threading.Thread(target=baggrundsopgave)
+thread.daemon = True  # gør tråden til daemon, så den stopper når appen stopper
+thread.start()
 
 # Homescreen
 @app.route('/')
 def home():
     return render_template("home.html")
 
-
 # Lokaler
 @app.route('/lokaler')
 def lokaler():
-    conn = sqlite3.connect('items.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT klasse, sensor_id, tidspunkt, co2, temperature, luftfugtighed FROM items")
-    rows = cursor.fetchall()
-    conn.close()
+    #conn = sqlite3.connect('items.db')
+    #cursor = conn.cursor()
+    #cursor.execute("SELECT klasse, sensor_id, tidspunkt, co2, temperature, luftfugtighed FROM items")
+    #rows = cursor.fetchall()
+    #conn.close()
 
-    return render_template("lokaler.html", rows=rows)
+    return render_template("lokaler.html", lokaler=lokaler_data)
 
 # Varmest/koldest
 @app.route('/varmest-koldest')
